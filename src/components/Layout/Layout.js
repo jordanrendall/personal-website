6ecd47fc316391db65aace527b417214e7b37126
jordanrendall <thomasjrendall@gmail.com>
2019-05-10 05:48:31 -0400

    Updated layout component to useStaticQuery hook

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,16 +7,16 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import PageStyles from '../Page/PageStyles';
 import './Layout.css';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+const Layout = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -24,25 +24,25 @@ const Layout = ({ children }) => (
           }
         }
       }
-    `}
-    render={data => (
-      <PageStyles>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-        </div>
-        <Footer />
-      </PageStyles>
-    )}
-  />
-);
+    `
+  );
+  return (
+    <PageStyles>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </PageStyles>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
