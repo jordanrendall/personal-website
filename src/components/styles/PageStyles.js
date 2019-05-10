9d01cea024b86f3259155f395b4e25a0ac9dabcb
jordanrendall <thomasjrendall@gmail.com>
2019-05-09 20:37:47 -0400

    Created PageStyles styles component

---
+++ src/components/styles/PageStyles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+
+const PageStyles = styled.div`
+  background: rebeccapurple;
+`;
+
+export default PageStyles;
