3a9d6ab70611969fe590902e552a242ffb1b893f
jordanrendall <thomasjrendall@gmail.com>
2019-05-22 09:43:12 -0400

    Added pages for each blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,21 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 const { createFilePath } = require(`gatsby-source-filesystem`);
-
-// You can delete this file if you're not using it
-exports.onCreateNode = ({ node, getNode }) => {
-  if (node.internal.type === `MarkdownRemark`) {
-    console.log(
-      createFilePath({
-        node,
-        getNode,
-        basePath: `src/content`,
-        trailingSlash: false,
-      })
-    );
-  }
-};
-
+const path = require(`path`);
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
   if (node.internal.type === `MarkdownRemark`) {
@@ -31,9 +17,38 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: `slug`,
-      value: `${node.frontmatter.category}${slug}`,
+      value: slug,
     });
   }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      });
+    });
+  });
 };
 
 // const path = require('path');
