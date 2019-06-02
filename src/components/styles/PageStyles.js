acc480ede53b865410665d849b287939623a74e5
jordanrendall <thomasjrendall@gmail.com>
2019-06-02 10:30:42 -0400

    Updated styling for page

--- src/components/styles/PageStyles.js
+++ src/components/styles/PageStyles.js
@@ -1,35 +1,46 @@
 import styled from 'styled-components';
 
 const PageStyles = styled.div`
-  /* Main content area */
-  background-color: ${props => props.theme.mustard};
   display: flex;
   flex-direction: column;
+
+  background: ${props => props.theme.contentColourPrimary};
   min-height: 100vh;
-  font-family: Geneva, Arial;
+  font-family: 'Source Sans Pro', Roboto, Helvetica, sans-serif;
+
   * {
-    color: ${props => props.theme.black}; /*var(--text-color);*/
+    color: ${props => props.theme.contentColourSecondary};
+    line-height: 2rem;
   }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 3rem;
+  }
+
   main {
     display: grid;
     justify-content: center;
     align-items: center;
     padding: 20px;
+
     a {
-      color: ${props => props.theme.black};
+      color: ${props => props.theme.contentColourSecondary};
       text-decoration: none;
 
       &:visited {
-        color: ${props => props.theme.black};
+        color: ${props => props.theme.contentColourSecondary};
       }
       &:hover,
       :focus {
-        color: grey;
+        color: ${props => props.theme.otherColourPrimary};
       }
     }
   }
-
-  /* grid-template-columns: 1fr auto; */
 `;
 
 export default PageStyles;
