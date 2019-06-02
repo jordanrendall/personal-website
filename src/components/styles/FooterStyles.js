26710cbc8768f23b421377880a8aee6bf2975b17
jordanrendall <thomasjrendall@gmail.com>
2019-06-02 10:29:02 -0400

    Referenced general names for theme colours in footer

--- src/components/styles/FooterStyles.js
+++ src/components/styles/FooterStyles.js
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  background: ${props => props.theme.green};
+  background: ${props => props.theme.navColourPrimary};
   display: flex;
   justify-content: center;
-  color: ${props => props.theme.black};
+  color: ${props => props.theme.navColourSecondary};
   min-width: 100vw;
-  font-size: 0.8rem;
+  font-size: 1rem;
   align-self: flex-end;
   position: fixed;
   bottom: 0;
-  border-top: 1px solid ${props => props.theme.black};
+  border-top: 2px solid ${props => props.theme.navColourSecondary};
+  padding: 10px;
 `;
 
 export default StyledFooter;
