aa2472d4e874de74d3b45cbbec401420db6aad36
Jordan Rendall <thomasjrendall@gmail.com>
2020-06-06 09:17:28 -0400

    Added project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,16 +4,33 @@ import { Link } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/seo';
 import styled from 'styled-components';
-
+import { colours, sizes } from '../components/Utilities';
 const StyledProjects = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const StyledProject = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
   h1 {
     font-size: 2rem;
-    padding: 20px;
+    padding: ${sizes(4)};
+    text-align: center;
     font-weight: bold;
+  }
+
+  .project-link {
+    padding: ${sizes(2)};
+    border-radius: ${sizes(2)};
+    &:hover {
+      background: ${colours(0, 0)};
+      color: white;
+    }
   }
 `;
 
@@ -21,8 +38,23 @@ const OtherProjectsPage = () => (
   <Layout>
     <SEO title='Projects' />
     <StyledProjects>
-      <h1>Coming Soon!</h1>
-      <Link to='/'>Go back to the homepage</Link>
+      <StyledProject>
+        <h1>Did you feed the cat?</h1>
+        <p>
+          A fun application that explores the use of #NextJS, #Apollo, #MongoDB,
+          and #Graphql. The application quickly displays the last time a pet was
+          fed.
+        </p>
+        <p>
+          <a
+            target='_blank'
+            className='project-link'
+            href='https://didyoufeedthecat.jordanrendall.com'
+          >
+            Check it out
+          </a>
+        </p>
+      </StyledProject>
     </StyledProjects>
   </Layout>
 );
